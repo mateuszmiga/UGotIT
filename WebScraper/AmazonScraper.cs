b4801f4be1f02d1b1d72a5f1379ff7d8dedb95c3
Mateusz Miga <mateusz.miga11@gmail.com>
2023-02-20 14:16:12 +0100

    Added method to parsing product url

--- WebScraper/AmazonScraper.cs
+++ WebScraper/AmazonScraper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,25 @@ namespace WebScraper
 {
     public class AmazonScraper : IDataScraper
     {
+        private const string BaseUrl = "https://www.amazon.pl/s?k=";
+        
+
         public ICollection<Review> GetReviews(string productName)
         {
+            var web = new HtmlWeb();
+            var doc = web.Load(ReturnProductPageUrl(productName));
+
+            var content = doc.QuerySelectorAll("span[\"global-reviews-all\"]");
             throw new NotImplementedException();
+        }
+
+        private static string ReturnProductPageUrl(string productName) 
+        {
+            var web = new HtmlWeb();
+            var formattedProductName = productName.Replace(' ', '+');
+            
+            var doc = web.Load(BaseUrl + formattedProductName);
+            var url = doc.DocumentNode.SelectNodes("//*[@id=\"search\"]/div[1]/div[1]/div/span[1]/div[1]/div[6]/div/div/div/div/div[2]/div[1]/h2/a");
         }
     }
 }
