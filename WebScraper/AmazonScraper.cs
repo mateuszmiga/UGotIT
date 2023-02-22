388b0c42fcf0d5a22b00fc693bf8f9d306ffaa57
Mateusz Miga <mateusz.miga11@gmail.com>
2023-02-22 12:49:25 +0100

    Updated ReturnProductPageUrl method

--- WebScraper/AmazonScraper.cs
+++ WebScraper/AmazonScraper.cs
@@ -10,8 +10,8 @@ namespace WebScraper
 {
     public class AmazonScraper : IDataScraper
     {
-        private const string BaseUrl = "https://www.amazon.pl/s?k=";
-        
+        private const string BaseUrl = "https://www.ceneo.pl/;szukaj-";
+        private const string BaseUrlParam = "?nocatnarrow=1";
 
         public ICollection<Review> GetReviews(string productName)
         {
@@ -27,16 +27,45 @@ namespace WebScraper
             var web = new HtmlWeb();
             var formattedProductName = productName.Replace(' ', '+');
 
+            var ceneoSearchResultPage = web.Load(BaseUrl + formattedProductName + BaseUrlParam);
+            var url = ceneoSearchResultPage.DocumentNode.SelectNodes("//*[@id=\"body\"]/div/div/div[3]/div/section/div[3]/div[1]/div/div[2]/div[1]/div[1]/div[1]/strong/a").FirstOrDefault();
+
+
+            var ceneoProductUrl = $"https://www.ceneo.pl{url.QuerySelector("a").Attributes["href"].Value}";
+
+            var ceneoProductPage = web.Load(ceneoProductUrl);
             try
             {
-                var doc = web.Load(BaseUrl + formattedProductName);
-                var url = doc.DocumentNode.SelectNodes("//*[@id=\"search\"]/div[1]/div[1]/div/span[1]/div[1]/div[6]/div/div/div/div/div[2]/div[1]/h2/a").FirstOrDefault();
-                return url.QuerySelector("a").Attributes["href"].Value;
+                var productNodes = ceneoProductPage.QuerySelectorAll("#click > div:nth-child(2) > section.product-offers.product-offers--standard > ul > li ");
+
+                if (productNodes.Any(n => n.OuterHtml.Contains("amazon")))
+                {
+                    var amazonNodeUrl = productNodes.Where(o => o.OuterHtml.Contains("amazon"))
+                .FirstOrDefault()
+                .QuerySelector("a").Attributes["href"].Value;
+
+                    var redirectionPage = "https://www.ceneo.pl" + amazonNodeUrl;
+                    var amazonProductPage = web.Load(redirectionPage)
+                        .QuerySelectorAll("meta")
+                        .Where(s => s.OuterHtml.Contains("amazon"))
+                        .First()
+                        .OuterHtml
+                        .Split("url=")
+                        .FirstOrDefault(s => s.Contains("amazon"))
+                        .Split("?")
+                        .First();
+                    return amazonProductPage;
+                }
+                else
+                {
+                    return "cannot find product on Amazon. Try to precise your search keyword";
+                }
             }
-            catch (Exception)
+
+            catch (NullReferenceException)
             {
-                return "can't find product!";
-            }            
+                return "cannot find product on Amazon. Try to precise your search keyword";
+            }
         }
     }
 }
