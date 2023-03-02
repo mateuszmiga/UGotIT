07919e7f1c683638848ff67f5a2282300e7bbe75
Mateusz Miga <mateusz.miga11@gmail.com>
2023-03-02 13:01:21 +0100

    CeneoFinder Tests with improvment of FindProduct Method

---
+++ UGotIt.Tests/CeneoFinderTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebScraper.Finder;
+using WebScraper.Models;
+using Xunit.Abstractions;
+
+namespace UGotIt.Tests
+{
+    public class CeneoFinderTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public CeneoFinderTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+
+        [Theory]
+        [InlineData("Xbox series one")]
+        [InlineData("PHILIPS Ovi Smart")]
+        [InlineData("Brother InkBenefit Plus")]
+        [InlineData("Iphone 13 128gb")]
+        public void FindProduct_TrueProducts_ShouldReturnProducts(string productName)
+        {
+            //Arrange
+            var finder = new CeneoFinder();
+
+            //Act
+            var products = finder.FindProduct(productName);
+
+            output.WriteLine("Found : " + products.Count.ToString());
+            foreach (var item in products)
+            {
+                output.WriteLine(item.ProductName);
+            }
+
+            //Assert
+            products.Should().NotBeEmpty();
+            products.Should().NotBeNull();
+        }
+    }
+}
