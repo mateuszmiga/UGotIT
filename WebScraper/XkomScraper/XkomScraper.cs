ee5700178f23304594d83c4408fc16db88fbde31
Łukasz Winowicz <lwinowicz@gmail.com>
2023-03-04 16:27:23 +0100

    created XkomScraper with selenium - downloads all coments by push the button to load more comments

--- WebScraper/XkomScraper/XkomScraper.cs
+++ WebScraper/XkomScraper/XkomScraper.cs
@@ -1,12 +1,9 @@
 ﻿using HtmlAgilityPack;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WebScraper.Models;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
 using WebScraper.XkomScraper.Models;
+using WebScraper.Models;
 
 namespace WebScraper.XkomScraper
 {
@@ -56,5 +53,71 @@ namespace WebScraper.XkomScraper
                 return new List<WebScraper.Models.Review>(); //return empty collection
             }
         }
+
+        private ICollection<WebScraper.Models.Review> ExtractReviewsFromProductPageWithSelenium(string url)
+        {
+
+            if (url != string.Empty)
+            {
+                var reviews = new List<WebScraper.Models.Review>();
+
+                // HtmlAgilityPack
+                var web = new HtmlWeb();
+                var doc = web.Load(url);
+                var reviewNodes = doc.QuerySelector("div.sc-1re71we-0.fOThDJ > script");
+                Root rootObj = JsonConvert.DeserializeObject<Root>(reviewNodes.InnerHtml);
+
+                var number = rootObj.aggregateRating.reviewCount;
+                double loopsNumber = Math.Round(((double)(number - 15) / 15));
+
+                // Selenium
+                ChromeOptions options = new ChromeOptions();
+                options.AddArgument("--headless");
+
+                IWebDriver driver = new ChromeDriver(options);
+                driver.Navigate().GoToUrl(url);
+
+                IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+
+                for (int i = 0; i < loopsNumber; i++)
+                {
+                    js.ExecuteScript("document.querySelector('button.sc-15ih3hi-0.sc-6i4pc6-0.bVwUtI.sc-1lp3a37-7.gRZnCa').click();");
+                    System.Threading.Thread.Sleep(2000);
+                }
+
+                WebElement scriptElement = (WebElement)driver.FindElement(By.CssSelector("div.sc-1re71we-0.fOThDJ > script"));
+
+                string scriptContent = scriptElement.GetAttribute("innerHTML");
+
+                try
+                {
+                    Root reviewNodes_ = JsonConvert.DeserializeObject<Root>(scriptContent);
+
+                    foreach (var reviewNode in reviewNodes_.review)
+                    {
+                        var review = new WebScraper.Models.Review();
+                        review.UserName = reviewNode.author.name;
+                        review.ReviewContent = reviewNode.description;
+                        review.SourcePage = url;
+                        review.Rating = reviewNode.reviewRating.ratingValue.ToString();
+
+                        reviews.Add(review);
+                    }
+
+                    return reviews;
+                }
+                catch (Exception)
+                {
+
+                    return new List<WebScraper.Models.Review>();
+                }
+            }
+            else
+            {
+                return new List<WebScraper.Models.Review>(); //return empty collection
+            }
+            
+        }
+
     }
 }
