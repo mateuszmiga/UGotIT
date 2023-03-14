5d7af0e0e340406f00e63907d34c523315a63dda
Mateusz Miga <mateusz.miga11@gmail.com>
2023-03-14 14:02:55 +0100

    added reviews page

--- OpinionMVC/ApiClient.cs
+++ OpinionMVC/ApiClient.cs
@@ -1,20 +1,22 @@
 ﻿using Newtonsoft.Json;
 using Flurl;
 using Flurl.Http;
-using WebScraper.Models;
+
+using OpinionMVC.Models;
 
 namespace OpinionMVC
 {
     public class ApiClient
     {
-        private readonly string baseUrl = "https://localhost:7042/api/Product?productName=";
+        private readonly string baseUrlProductEndpoint = "https://localhost:7042/api/Product?productName=";
+        private readonly string baseUrlReviewEndpoint = "https://localhost:7042/api/Review?productUrl=";
         HttpClient client = new HttpClient();
 
         public async Task<List<Product>> GetProducts(string searchTerm)
         {
             try
             {
-                var response = await client.GetStringAsync(baseUrl + searchTerm);
+                var response = await client.GetStringAsync(baseUrlProductEndpoint + searchTerm);
                 var result = JsonConvert.DeserializeObject<List<Product>>(response);
 
                 return result;
@@ -23,6 +25,23 @@ namespace OpinionMVC
             catch (Exception)
             {
                 return new List<Product>();
+            }
+
+        }
+
+        public async Task<List<Review>> GetReviews(string productUrl)
+        {
+            try
+            {
+                var response = await client.GetStringAsync(baseUrlReviewEndpoint + productUrl);
+                var result = JsonConvert.DeserializeObject<List<Review>>(response);
+
+                return result;
+
+            }
+            catch (Exception)
+            {
+                return new List<Review>();
             }
 
         }
---
+++ OpinionMVC/Controllers/ReviewController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpinionMVC.Models;
+
+namespace OpinionMVC.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private readonly ApiClient _client;
+
+        public ReviewController(ApiClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<ActionResult> GetReviews(string product)
+        {
+            var result = await _client.GetReviews(product);
+            
+            return View(result);
+        }        
+    }
+}
