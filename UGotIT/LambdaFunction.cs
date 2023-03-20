ed0abfc5e6085fbd91bb4885ea5fa92eb4d31d9d
Mateusz Miga <mateusz.miga11@gmail.com>
2023-03-20 11:44:36 +0100

    aws

---
+++ UGotIT/LambdaFunction.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.Lambda.AspNetCoreServer;
+
+namespace UGotIT
+{
+    public class LambdaFunction : APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Program>()
+                .UseApiGateway();
+        }
+    }
+}
